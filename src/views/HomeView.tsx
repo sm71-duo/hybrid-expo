a4b68c6f0dc85639ae30b8aa75aaac4dd80adae1
Roy <roy.appeldoorn@gmail.com>
2021-09-29 13:00:10 +0200

    hook for agora

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,121 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, PermissionsAndroid, Platform, Button } from "react-native";
+import { Text, Button } from "react-native";
 import styled from "styled-components/native";
 import { spacing } from "../styles/styling";
-import RtcEngine from "react-native-agora";
-
-// Define a Props interface.
-interface Props {}
-
-// Define a State interface.
-interface State {
-  appId: string;
-  token: string;
-  channelName: string;
-  joinSucceed: boolean;
-  openMicrophone: boolean;
-  enableSpeakerphone: boolean;
-  peerIds: number[];
-}
+import useAgora from "../hooks/useAgora";
 
 const HomeView = () => {
-  const [appId] = useState("6e8c688be0734ab097c496f141dbc255");
-  const [token, setToken] = useState(null);
-  const [channelName, setChannelName] = useState("channel-x");
-  const [openMicrophone, setOpenMicrophone] = useState(true);
-  const [enableSpeakerphone, setEnableSpeakerphone] = useState(true);
-  const [joinSucceed, setJoinSucceed] = useState(false);
-  const [peerIds, setPeerIds] = useState([]);
-
-  let _engine: RtcEngine;
-
-  useEffect(() => {
-    if (Platform.OS === "android") {
-      // Request required permissions from Android
-      requestCameraAndAudioPermission().then(() => {
-        console.log("requested!");
-      });
-    }
-    init();
-    return () => {};
-  }, []);
-
-  const requestCameraAndAudioPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.requestMultiple([
-        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-      ]);
-      if (
-        granted["android.permission.RECORD_AUDIO"] ===
-        PermissionsAndroid.RESULTS.GRANTED
-      ) {
-        console.log("You can use the mic");
-      } else {
-        console.log("Permission denied");
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  };
-
-  const init = async () => {
-    _engine = await RtcEngine.create(appId);
-    await _engine.enableAudio();
-
-    _engine.addListener("UserJoined", (uid, elapsed) => {
-      console.log("UserJoined", uid, elapsed);
-      if (peerIds.indexOf(uid) === -1) {
-        setPeerIds({ ...peerIds, uid });
-      }
-    });
-
-    _engine.addListener("UserOffline", (uid, reason) => {
-      console.log("UserOffline", uid, reason);
-
-      const oldPeerIds = peerIds;
-      oldPeerIds.filter((id) => id !== uid);
-      setPeerIds(oldPeerIds);
-    });
-
-    _engine.addListener("JoinChannelSuccess", (channel, uid, elapsed) => {
-      console.log("JoinChannelSuccess", channel, uid, elapsed);
-      setJoinSucceed(true);
-    });
-  };
-
-  const joinChannel = async () => {
-    await _engine?.joinChannel(token, channelName, null, 0).then(() => {
-      console.log(token, channelName);
-    });
-  };
-
-  const switchMicrophone = () => {
-    _engine
-      ?.enableLocalAudio(!openMicrophone)
-      .then(() => {
-        setOpenMicrophone(!openMicrophone);
-      })
-      .catch((err) => {
-        console.warn("enableLocalAudio", err);
-      });
-  };
-
-  const switchSpeakerphone = () => {
-    _engine
-      ?.setEnableSpeakerphone(!enableSpeakerphone)
-      .then(() => {
-        setEnableSpeakerphone(!enableSpeakerphone);
-      })
-      .catch((err) => {
-        console.warn("setEnableSpeakerphone", err);
-      });
-  };
-
-  const leaveChannel = async () => {
-    await _engine?.leaveChannel();
-    setPeerIds([]);
-    setJoinSucceed(false);
-  };
+  const {
+    switchMicrophone,
+    switchSpeakerphone,
+    leaveChannel,
+    joinChannel,
+    joinSucceed,
+    openMicrophone,
+    enableSpeakerphone,
+  } = useAgora();
 
   return (
     <Wrapper>
@@ -128,7 +26,10 @@ const HomeView = () => {
         title={`Microphone ${openMicrophone ? "on" : "off"}`}
         onPress={switchMicrophone}
       />
-      <Button title="test" onPress={requestCameraAndAudioPermission} />
+      <Button
+        title={`Speaker phone ${enableSpeakerphone ? "on" : "off"}`}
+        onPress={switchSpeakerphone}
+      />
     </Wrapper>
   );
 };
